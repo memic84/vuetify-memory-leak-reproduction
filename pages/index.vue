d5c0b28ec505b76437827b22b0d168e02e6d1082
Sead Memic <sead.memic@intracto.com>
2023-11-22 20:50:41 +0100

    reproduction

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <v-list lines="one">
+          <v-list-item
+                  v-for="n in 3"
+                  :key="n"
+                  :title="'Item ' + n"
+                  subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+          ></v-list-item>
+
+          <v-btn>Hello</v-btn>
+      </v-list>
+  </div>
+</template>
